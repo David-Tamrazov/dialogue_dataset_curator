821a255d8e8a6046ee0fb34d1ea789f4738d4a0c
David Tamrazov <david.v.tamrazov@gmail.com>
2017-09-18 15:49:42 -0400

    Finished notification service

--- apple.py
+++ apple.py
@@ -7,47 +7,68 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-url = 'https://communities.apple.com/fr/message/' + str(randint(220010632, 220039330))
+def main():
+    # url = 'https://communities.apple.com/fr/message/' + str(randint(220010632, 220039330))
+    url = 'https://communities.apple.com/de/message/200031820#200031820'
 
-print(url)
+    print(url)
 
-response = urllib.urlopen(url).read()
-# print response[:1000]
-print "\n"
-# print(type(response))
-soup2 = BeautifulSoup.BeautifulSoup(response, "lxml")
+    response = urllib.urlopen(url).read()
+    # print response[:1000]
+    print "\n"
 
-# print(type(soup2))
-searchterm = ["j-thread-post", "all-replies-container"]
-# print searchterm[0]
+    # print(type(response))
+    soup = BeautifulSoup.BeautifulSoup(response, "lxml")
 
-for p in range(0, 2):
-    section = str(soup2.find("div", {"class": searchterm[p]}))
-    soup3 = BeautifulSoup.BeautifulSoup(section, "lxml")
+    get_post_author(soup)
+    get_usernames_of_replies(soup)
 
-    # print((soup3))
-    # print("test")
+    # # print(type(soup2))
+    # searchterm = ["j-thread-post", "all-replies-container"]
 
-    usernameSoup = soup3.find(text='data-userid')
+    # print searchterm[0]
 
-    userSoup = soup3.findAll("div", {"class": "jive-rendered-content"})  # , limit=10
+    # for p in range(0, 2):
+    #     section = str(soup.find("div", {"class": searchterm[p]}))
+    #     soup2 = BeautifulSoup.BeautifulSoup(section, "lxml")
 
-    # print usernameSoup
+    #     # print((soup3))
+    #     # print("test")
 
-    length = len(userSoup)
+    #     usernameSoup = soup.find(text='data-userid')
 
-    # print(length)
+    #     userSoup = soup.findAll("div", {"class": "jive-rendered-content"})  # , limit=10
 
-    for x in range(0, length):
-        t = userSoup[x]
+    #     # print usernameSoup
 
-        lol = t.findAll("p")
-        length2 = len(lol)
+    #     length = len(userSoup)
 
-        for z in range(0, length2):
-            y = lol[z]
-            print(y.text)
+    #     # print(length)
 
-        print "\n\n"
+    #     for x in range(0, length):
+    #         t = userSoup[x]
 
-        # print usernames
+    #         lol = t.findAll("p")
+    #         length2 = len(lol)
+
+    #         for z in range(0, length2):
+    #             y = lol[z]
+    #             print(y.text)
+
+    #         print "\n\n"
+
+    #         # print usernames
+
+def get_post_author(soup):
+    div_data = soup.find("div", "j-post-author")
+    link_data = div_data.find("a").attrs
+    author = link_data['data-username']
+    return author
+
+def get_usernames_of_replies(soup):
+    replies = soup.findAll("a", attrs={'class': 'j-avatar'})
+    username_list = [ reply.attrs['data-username'] for reply in replies]
+    return username_list
+
+if __name__ == '__main__':
+    main()
